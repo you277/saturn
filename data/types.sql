d5d3cc96e786cf6db97454690b89df613be83bd8
you <144391848+you277@users.noreply.github.com>
2025-03-28 14:36:00 -0400

    Create types.sql

---
+++ data/types.sql
@@ -0,0 +1,6 @@
+INSERT INTO assignment_types (id,name) VALUES (1, 'Minor Assessment');
+INSERT INTO assignment_types (id,name) VALUES (2, 'Major Assessment');
+
+INSERT INTO course_types (id,name) VALUES (1, 'Elective');
+INSERT INTO course_types (id,name) VALUES (2, 'Regents');
+INSERT INTO course_types (id,name) VALUES (3, 'AP');
