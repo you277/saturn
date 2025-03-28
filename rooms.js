ffbc9c77ccab512f27bd2a91e908c07ab87c3ab7
you <144391848+you277@users.noreply.github.com>
2025-03-28 14:30:01 -0400

    Create rooms.js

---
+++ rooms.js
@@ -0,0 +1,16 @@
+let inserts = []
+let floorSections = ["N","W","E","S","C"]
+
+let roomId = 1
+
+for (let f = 0; f < 8; f++) {
+	let floor = f == 0 ? "B" : f
+	for (let i = 1; i <= 20; i++ ) {
+		for (let section of floorSections) {
+			inserts.push(`INSERT INTO rooms (id,name) VALUES (${roomId},'${floor}${section}${i}');`)
+            roomId += 1
+		}
+	}
+}
+
+console.log(inserts.join("\n"))
